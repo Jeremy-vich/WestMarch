80b9829bfa5bdc8439bd2e300bf9ed2bf11b9ae9
Jérémy Vich <vichjeremy@gmail.com>
2024-12-03 13:11:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,43 +4,13 @@
 import { ChatHooks } from './modules/chat.js';
 import { ImageHooks } from './modules/image.js';
 import { PlayerHooks } from './modules/player.js';
+import { ScenesHooks } from './modules/scenes.js';
 
 Hooks.on("init", () => {
     ChatHooks();
     ImageHooks();
     PlayerHooks();
+    ScenesHooks();
 });
 
 //----------------------------------------------
-
-Hooks.on('getSceneNavigationContext', (html, contextMenu) => {
-    contextMenu.push({
-        name: "Go With Party",
-        icon: '<i class="fa-solid fa-people-arrows"></i>',
-        callback: li => {
-            var destination = game.scenes.get(li.data("sceneId"));
-            GoWithParty(destination);
-        },
-        condition: li => game.user.isGM
-    });
-});
-
-Hooks.on('getSceneDirectoryEntryContext', (html, contextMenu) => {
-    contextMenu.push({
-        name: "Go With Party",
-        icon: '<i class="fa-solid fa-people-arrows"></i>',
-        callback: li => {
-            var destination = game.scenes.get(li.data("documentId"));
-            GoWithParty(destination);
-        },
-        condition: li => game.user.isGM
-    });
-});
-
-function GoWithParty(destination){
-    game.users.forEach(user => { 
-        if (user.getFlag("westmarch", "partyId") == game.user.getFlag("westmarch", "partyId")) {
-            game.socket.emit("pullToScene", destination.id, user.id);
-        }
-    });
-}
